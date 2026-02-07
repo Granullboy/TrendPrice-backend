5e13e1d18b7bfc9c89a935a4cb01350756ae6d48
Granullboy <granulboy@gmail.com>
2026-02-07 21:38:32 +0500

    add catalog service v2

---
+++ catalog-bff-service/src/main/java/kz/trendprice/server/catalogbffservice/CatalogBffServiceApplication.java
@@ -0,0 +1,13 @@
+package kz.trendprice.server.catalogbffservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogBffServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogBffServiceApplication.class, args);
+	}
+
+}
---
+++ catalog-bff-service/src/test/java/kz/trendprice/server/catalogbffservice/CatalogBffServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.trendprice.server.catalogbffservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogBffServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
